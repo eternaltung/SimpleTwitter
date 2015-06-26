9c98ecfa1480e21364b3efe932b49508a6dab303
eternaltung <eternaltung@gmail.com>
2015-06-26 14:59:00 +0800

    Initial Commit

---
+++ SimpleTwitter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SimpleTwitter
+//
+//  Created by Shih-Ming Tung on 6/26/15.
+//  Copyright (c) 2015 Shih-Ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SimpleTwitter/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SimpleTwitter
+//
+//  Created by Shih-Ming Tung on 6/26/15.
+//  Copyright (c) 2015 Shih-Ming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
